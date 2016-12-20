3d315bcb04bc7c494c3cb3345900bf37ba79e1ad
andrewdbass <andybass12@gmail.com>
2016-12-19 23:03:14 -0700

    more ui changes

--- src/app/podcasts/podcasts.component.ts
+++ src/app/podcasts/podcasts.component.ts
@@ -17,9 +17,18 @@ export class PodcastsComponent implements OnInit {
   public tagsValue =[];
 
   public emptyStateMessages =[
-    "Bored at work? Set the timer and we will serve up some awesome content.",
-    "Sitting on the john? Pick a time and we will find the perfect thing to listen to.",
-    "At a party and wishing you weren't? Choose a time and we will find something for you to do."
+    {
+      title: "Bored at work?",
+      message: "Set the timer and we will serve up some awesome content."
+    },
+    {
+      title: "Sitting on the john?",
+      message: "Pick a time and we will find the perfect thing to read."
+    },
+    {
+      title: "At a party and wishing you weren't?",
+      message: "Choose a time and we will find something for you to do."
+    }
   ]
   public emptyStateMessageIndex = 0;
   private podcasts = [];
